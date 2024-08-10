78598c2235a51240cc5eb35c5d14747d950bfeed
Miss shelby <ceeyntia@gmail.com>
2024-08-10 04:46:54 +0100

    'updated projectid'

--- src/app/(pages)/payment/page.js
+++ src/app/(pages)/payment/page.js
@@ -96,7 +96,7 @@ console.log(formFields);
         setLoading(false)
         router.push(response.data?.data?.authorization_url);
         // toast.success(message);
-    
+        Cookies.set(projectId,"projectId")
         console.log(response.data?.data?.authorization_url);
       })
       .catch(function (error) {
